3b2511ade7ce6b815b3bf3f6ce6c602a739fd3db
Nicholas Meyer <nixton.meyer@gmail.com>
2025-03-06 16:10:34 +0100

    Added back non-gpu PolygonFilter #minor

--- src/pchandler/geometry/filters.py
+++ src/pchandler/geometry/filters.py
@@ -5,11 +5,16 @@ Provides functions that filter or extract subsets from a PointCloudData instance
 """
 from abc import ABC, abstractmethod
 import logging
+from typing import Callable, Tuple, Optional
 import warnings
 
+import alphashape
 import numpy as np
-from typing import Callable, Tuple, Optional
-from shapely.geometry import Polygon, Point
+from shapely.geometry import Polygon, Point, MultiPolygon
+from shapely.affinity import scale, translate
+from shapely.prepared import prep
+from shapely import contains_xy
+# from shapely.vectorized import contains
 from numpy.typing import NDArray
 
 from .core import PointCloudData
@@ -155,12 +160,18 @@ class PolygonFilter(PointCloudFilter):
 
     def mask(self, pcd: PointCloudData) -> NDArray[np.bool_]:
         if self.plane == 'xy':
-            proj_pts = pcd.xyz[:, :2]
+            dims = [0, 1]
         elif self.plane == 'xz':
-            proj_pts = pcd.xyz[:, [0, 2]]
+            dims = [0, 2]
         else:
-            proj_pts = pcd.xyz[:, 1:]
+            dims = [1, 2]
 
+        polygon = self.polygon if pcd.global_coordinate_shift is None else (
+            translate(self.polygon, *(-1*pcd.global_coordinate_shift[dims])))
+
+        mask = contains_xy(polygon, pcd.xyz[:, dims[0]], pcd.xyz[:, dims[1]])
+
+        return mask
 
 class ScalarFieldFilter(PointCloudFilter):
     def __init__(self, field_label: str, lower_bound: float = -np.inf, upper_bound: float = np.inf):
@@ -317,6 +328,74 @@ class VoxelDownsamplingFilter:
 
 
 
+def get_outline_polygon(pcd: PointCloudData, plane: str, alpha_value: float = 10.0, nb_points: int = -1) -> Polygon:
+    """
+    Computes the outline of the point cloud as a polygon in a specific 2D projection.
+
+    Parameters
+    ----------
+    plane : str
+        The plane of projection ('xy', 'xz', or 'yz').
+    alpha_value : float, default=10.0
+        The alpha value for the alpha shape algorithm, controlling the detail of the outline.
+    nb_points : int, default=-1
+        The number of points to use for the computation. If -1, all points are used.
+
+    Returns
+    -------
+    Polygon
+        A Shapely Polygon representing the outline of the point cloud.
+
+    Raises
+    ------
+    ValueError
+        If the specified plane is invalid.
+    NotImplementedError
+        If the outline computation results in an unsupported geometry type.
+    """
+    match plane:
+        case 'xy':
+            proj_pts = pcd.xyz[:, :2]
+        case 'xz':
+            proj_pts = pcd.xyz[:, [0, 2]]
+        case 'yz':
+            proj_pts = pcd.xyz[:, 1:]
+        case _:
+            raise ValueError
+
+    # Normalize the points
+    proj_pts_mean = proj_pts.mean(axis=0)
+    proj_pts_scale = proj_pts.max(axis=0) - proj_pts.min(axis=0)
+    proj_pts_norm = (proj_pts - proj_pts_mean) / proj_pts_scale
+    if nb_points > 0:
+        proj_pts_norm = proj_pts_norm[np.random.permutation(proj_pts_norm.shape[0])[:nb_points], :]
+
+    # Add noise to reduce risk of underdefined dimensionality
+    noise = np.random.normal(scale=1e-6, size=proj_pts_norm.shape)
+    proj_pts_norm = proj_pts_norm + noise
+
+    als_norm = alphashape.alphashape(proj_pts_norm, alpha=alpha_value)
+    als = translate(scale(als_norm, *proj_pts_scale), *proj_pts_mean)
+
+    if isinstance(als, MultiPolygon):
+        als = max(als.geoms, key=lambda polygon: polygon.area)
+
+    if not isinstance(als, Polygon):
+        raise NotImplementedError
+
+    if pcd.global_coordinate_shift is not None:
+        match plane:
+            case 'xy':
+                gs = pcd.global_coordinate_shift[:2]
+            case 'xz':
+                gs = pcd.global_coordinate_shift[[0, 2]]
+            case 'yz':
+                gs = pcd.global_coordinate_shift[1:]
+            case _:
+                raise ValueError
+        als = translate(als, *gs)
+
+    return als
 
 
 
