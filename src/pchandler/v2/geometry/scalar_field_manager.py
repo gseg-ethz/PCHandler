fix: Remove auto normalisation pf intensity/scalarfields for more flexibility at Data I/O

src/pchandler/v2/geometry/scalar_field_manager.py
@@ -121,11 +121,11 @@ class ScalarFieldManager(MutableMapping[str, SF_T]):
             value = value.arr
 
         if name == INTENSITY_FIELD:
-            self.fields[name] = NormalisedInt16ScalarField(value, name=INTENSITY_FIELD, origin_dtype=origin_dtype)
+            self.fields[name] = ScalarField(value, name=INTENSITY_FIELD, origin_dtype=origin_dtype)
             return None
 
         if name == REFLECTANCE_FIELD:
-            self.fields[name] = NormalisedInt16ScalarField(value, name=REFLECTANCE_FIELD, origin_dtype=origin_dtype)
+            self.fields[name] = ScalarField(value, name=REFLECTANCE_FIELD, origin_dtype=origin_dtype)
             return None
 
         if name in RGB_ALL_POTENTIAL_NAMES:
@@ -202,7 +202,7 @@ class ScalarFieldManager(MutableMapping[str, SF_T]):
     @intensity.setter
     def intensity(self, value: np.ndarray | ScalarField):
         if isinstance(value, np.ndarray):
-            value = NormalisedInt16ScalarField(value, name=INTENSITY_FIELD)
+            value = ScalarField(value, name=INTENSITY_FIELD)
         self.add_field(value)
 
     @property
@@ -212,7 +212,7 @@ class ScalarFieldManager(MutableMapping[str, SF_T]):
     @reflectance.setter
     def reflectance(self, value: np.ndarray | ScalarField):
         if isinstance(value, np.ndarray):
-            value = NormalisedInt16ScalarField(value, name=REFLECTANCE_FIELD)
+            value = ScalarField(value, name=REFLECTANCE_FIELD)
         self.add_field(value)
 
     @validate_call(config=DEFAULT_CONFIG)

tests/data_io/test_las.py
@@ -4,14 +4,26 @@ from pathlib import Path
 
 import numpy as np
 
-from pchandler.v2.data_io.las import LasHandler
+from pchandler.v2.geometry import PointCloudData
+from pchandler.v2.data_io import Las as LAS
+
+
+@pytest.fixture(scope='function')
+def pcd():
+    return PointCloudData(
+        np.random.rand(1000, 3) * 100,
+        rgb=np.random.randint(0, 256, (1000, 3), dtype=np.uint8),
+        intensity=np.random.randint(0, 256, (1000,), dtype=np.uint8),
+        reflectance=np.random.randint(0, 256, (1000,), dtype=np.uint8),
+        normals=np.random.rand(1000, 3).astype(np.float32),
+    )
 
 
 class TestLasHandler:
     rgb_file = Path(r"D:\Python\pchandler\tests\data\test_target_intensity_normals_rgb.las")
 
     def test_load(self):
-        pcd = LasHandler.load(self.rgb_file)
+        pcd = LAS.load(self.rgb_file)
         assert len(pcd) == 43577
         assert 'intensity' in pcd.scalar_fields
         assert 'normals' in pcd.scalar_fields
@@ -19,9 +31,9 @@ class TestLasHandler:
 
     def test_save(self):
         out_path = Path(r"D:\Python\pchandler\tests\data\test_target_rgb_temp.las")
-        original_pcd = LasHandler.load(self.rgb_file)
-        LasHandler.save(original_pcd, out_path)
-        new_pcd = LasHandler.load(out_path)
+        original_pcd = LAS.load(self.rgb_file)
+        LAS.save(original_pcd, out_path)
+        new_pcd = LAS.load(out_path)
 
         assert np.allclose(original_pcd.xyz, new_pcd.xyz, atol=0.0001)
         assert np.allclose(original_pcd.normals, new_pcd.normals, atol=0.0001)

tests/geometry/test_core_geometry.py
@@ -29,17 +29,12 @@ def normals_() -> np.ndarray:
 
 @pytest.fixture(scope="function", autouse=True)
 def intensity_() -> np.ndarray:
-    return np.random.rand(N)
+    return np.random.rand(N).astype(np.float32)
 
 
 @pytest.fixture(scope="function", autouse=True)
 def reflectance_() -> np.ndarray:
-    return np.random.rand(N)
-
-
-@pytest.fixture(scope="function", autouse=True)
-def intensities_() -> np.ndarray:
-    return np.random.rand(100).astype(np.float32)
+    return np.random.rand(N).astype(np.float32)
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -142,14 +137,14 @@ class TestPointCloudData:
 
         def test_intensity_keyword(self, xyz_, intensity_):
             pcd = PointCloudData(xyz_, intensity=intensity_)
-            vals = NormalisedInt16ScalarField(intensity_, name='test')
+            vals = ScalarField(intensity_, name='test')
             assert "intensity" in pcd.scalar_fields
             assert np.allclose(pcd.intensity, vals)
 
         def test_reflectance_keyword(self, xyz_, intensity_):
             pcd = PointCloudData(xyz_, reflectance=intensity_)
 
-            vals = NormalisedInt16ScalarField(intensity_, name='test')
+            vals = ScalarField(intensity_, name='test')
             assert "reflectance" in pcd.scalar_fields
             assert np.allclose(pcd.reflectance, vals)
 
@@ -263,12 +258,12 @@ class TestPointCloudData:
         def test_intensity_getter(self, pcd):
             assert isinstance(pcd.intensity, ScalarField)
             assert pcd.intensity.ndim == 1
-            assert pcd.intensity.dtype == np.int16
+            assert pcd.intensity.dtype == np.float32
 
         def test_reflectance_getter(self, pcd):
             assert isinstance(pcd.reflectance, ScalarField)
             assert pcd.reflectance.ndim == 1
-            assert pcd.reflectance.dtype == np.int16
+            assert pcd.reflectance.dtype == np.float32
 
         def test_rgb_setter(self, pcd):
             new_data = np.random.randint(0, 255, (N, 3), dtype=np.uint8)
@@ -287,19 +282,19 @@ class TestPointCloudData:
             assert np.all(pcd.normals == new_data)
 
         def test_intensity_setter(self, pcd):
-            new_data = np.random.rand(N)
+            new_data = np.random.rand(N).astype(np.float32)
             pcd.intensity = new_data
             assert isinstance(pcd.intensity, ScalarField)
             assert pcd.intensity.ndim == 1
-            assert pcd.intensity.dtype == np.int16
+            assert pcd.intensity.dtype == np.float32
             assert np.allclose(pcd.intensity.get_original_data(), new_data, atol=1/2**16)
 
         def test_reflectance_setter(self, pcd):
             new_data = np.random.rand(N)
-            pcd.reflectance = new_data
+            pcd.reflectance = new_data.astype(np.float32)
             assert isinstance(pcd.reflectance, ScalarField)
             assert pcd.reflectance.ndim == 1
-            assert pcd.reflectance.dtype == np.int16
+            assert pcd.reflectance.dtype == np.float32
             assert np.allclose(pcd.reflectance.get_original_data(), new_data, atol=1/2**16)
 
         def test_sf_input_as_scalar_fields(self, xyz_, rgb_, normals_):

tests/geometry/test_scalar_field_manager.py
@@ -259,19 +259,17 @@ class TestNamedFieldPropertyGetters:
         array = np.random.rand(N)
         empty_sfm.intensity = array
 
-        assert np.all(empty_sfm.intensity != array)
         assert hasattr(empty_sfm, "intensity")
-        assert np.allclose(empty_sfm.intensity.get_original_data(), array, atol=1/2**16)
-        assert empty_sfm.intensity.dtype == np.int16
+        assert np.allclose(empty_sfm.intensity, array)
+        assert empty_sfm.intensity.dtype == array.dtype
 
     def test_reflectance_setter(self, empty_sfm):
         array = np.random.rand(N)
         empty_sfm.reflectance = array
 
         assert hasattr(empty_sfm, "reflectance")
-        assert np.all(empty_sfm.reflectance != array)
-        assert np.allclose(empty_sfm.reflectance.get_original_data(), array, atol=1/2**16)
-        assert empty_sfm.reflectance.dtype == np.int16
+        assert np.allclose(empty_sfm.reflectance, array)
+        assert empty_sfm.reflectance.dtype == array.dtype
 
 
 class TestShapeHelpers: