fix: Added back missing FoV features (Conversion between angular units, ...). Added link to coordinates.fov

src/pchandler/v2/geometry/coordinates.py
@@ -9,6 +9,7 @@ import numpy as np
 import numpy.typing as npt
 from pydantic import BeforeValidator, ConfigDict, Field, validate_call
 
+from .fov import FoV
 from ..base_arrays import ArrayNx2, ArrayNx3, FixedLengthArray
 from ..base_types import Array_4x4_T, Vector_3_T, Array_Nx3_T, Array_3x3_T
 from ..constants import HALF_PI, PI, TWO_PI, DEFAULT_CONFIG
@@ -143,9 +144,9 @@ class CartesianCoordinates(Abstract3dCoordinates):
     def _hz_v(self) -> npt.NDArray[np.floating]:
         return self.rhv[:, 1:]
 
-    @property
-    def fov(self):  # TODO must implement this
-        raise NotImplementedError
+    @cached_property
+    def fov(self) -> FoV:
+        return FoV.from_angles(self.hz, self.v)
 
     def to_spherical(self) -> SphericalCoordinates:
         spherical = SphericalCoordinates(**self.model_dump(exclude={"arr"}) | {"arr": self.spher})
@@ -179,9 +180,9 @@ class CartesianCoordinates(Abstract3dCoordinates):
 class SphericalCoordinates(Abstract3dCoordinates):
     arr: Annotated[Array_Nx3_T, Field(validation_alias="spher"), BeforeValidator(validate_spherical_angles)]
 
-    @property
-    def fov(self) -> Any:
-        raise NotImplementedError
+    @cached_property
+    def fov(self) -> FoV:
+        return FoV.from_angles(self.hz, self.v)
 
     @property
     def spher(self) -> npt.NDArray[np.floating]:

src/pchandler/v2/geometry/fov.py
@@ -74,6 +74,7 @@ import warnings
 from dataclasses import dataclass, field
 from fractions import Fraction
 from itertools import chain
+from functools import partial
 from typing import Iterable, Optional, cast, Self, Annotated, NamedTuple, TYPE_CHECKING, Any
 
 import numpy as np
@@ -85,6 +86,7 @@ from ..util import AngleUnit, convert_angles
 
 if TYPE_CHECKING:
     from .coordinates import SphericalCoordinates, CartesianCoordinates
+    from ..base_types import Vector_3_T
 
 logger = logging.getLogger(__name__.split(".")[0])
 
@@ -107,6 +109,39 @@ class FoV(NamedTuple):
     top: VAngleT
     right: HzAngleT
     bottom: VAngleT
+
+    # def __new__(
+    #         cls,
+    #         *,
+    #         left: HzAngleT,
+    #         top: VAngleT,
+    #         right: HzAngleT,
+    #         bottom: VAngleT,
+    #         input_unit: AngleUnit = AngleUnit.RAD
+    # ):
+    #     values = np.array([left, top, right, bottom], dtype=float)
+    #     convert_angles(values, source_unit=input_unit, target_unit=AngleUnit.RAD, out=values)
+    #     super(FoV, cls).__new__(cls, values[0], values[1], values[2], values[3])
+
+
+    @classmethod
+    def from_unit(
+            cls,
+            *,
+            left: HzAngleT,
+            top: VAngleT,
+            right: HzAngleT,
+            bottom: VAngleT,
+            input_unit: AngleUnit = AngleUnit.RAD
+    ) -> Self:
+        values = np.array([left, top, right, bottom], dtype=float)
+        l, t, r, b = convert_angles(values, source_unit=input_unit, target_unit=AngleUnit.RAD).tolist()
+        return cls(left = l, top = t, right = r, bottom = b)
+
+    @classmethod
+    def from_angles(cls, horizontal: Vector_3_T, vertical: Vector_3_T) -> Self:
+        return cls(left=horizontal.min(), top=vertical.min(), right=horizontal.max(), bottom=vertical.max())
+
 
     def __iter__(self) -> Iterable[HzAngleT | VAngleT]:
         yield self.left
@@ -222,9 +257,14 @@ class FoV(NamedTuple):
         return self.extent()[0] / self.extent()[1]
 
     def __repr__(self):
+        # return (
+        #     f"({self.left:0.4f}, {self.top:0.4f}, "
+        #     f"{self.right:0.4f}, {self.bottom:0.4f})"
+        # )
+
+        left, top, right, bottom = tuple(map(partial(convert_angles, source_unit = AngleUnit.RAD, target_unit = AngleUnit.GON), self))
         return (
-            f"({self.left:0.4f}, {self.top:0.4f}, "
-            f"{self.right:0.4f}, {self.bottom:0.4f})"
+            f"({left=:0.4f}, {right=:0.4f}, {top=:0.4f},{bottom=:0.4f}; in {AngleUnit.GON})"
         )
 
     @validate_call(config=DEFAULT_CONFIG)