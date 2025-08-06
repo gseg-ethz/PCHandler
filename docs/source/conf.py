5426dfbc686a2da415a0e09ec7d9dbdf216e3c29
Nicholas Meyer <nixton.meyer@gmail.com>
2025-08-06 13:33:32 +0200

    ci: automatic version update via release-please for sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@ import sys
 
 # from docs.conf import autosummary_generate
 
-sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath("../../src"))
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -13,30 +13,32 @@ sys.path.insert(0, os.path.abspath('../../src'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'PCHandler'
-copyright = '2024, Nicholas Meyer'
-author = 'Nicholas Meyer'
-release = '0.6.5'
+project = "PCHandler"
+copyright = "2024, Nicholas Meyer"
+author = "Nicholas Meyer"
+
+# Automatic version updates via release-please
+version = "1.0.1"  # x-release-please-version
+release = "1.0.1"  # x-release-please-version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',  # For generating documentation from docstrings
-    'sphinx.ext.napoleon',  # For Google-style and NumPy-style docstrings
-    'sphinx.ext.autosummary',  # For summary tables
+    "sphinx.ext.autodoc",  # For generating documentation from docstrings
+    "sphinx.ext.napoleon",  # For Google-style and NumPy-style docstrings
+    "sphinx.ext.autosummary",  # For summary tables
     # 'sphinx_autodoc_typehints',  # For type hints
 ]
 
 autosummary_generate = True
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
